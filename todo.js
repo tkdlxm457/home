7fb2993585fcd85a2cc062177f6dbaafec5ae378
김성현 <seonghyun.kim@ikoob.com>
2020-06-01 10:31:05 +0900

    주석정비

--- todo.js
+++ todo.js
@@ -1,20 +1,38 @@
+////////////////////////////////////////////////
+// !let과 const를 공부 해 놓고 왜 var를 쓰는건지.....?
+// !주석을 달아주세요 제발 부탁합니다.
+////////////////////////////////////////////////
 
 // Create a "close" button and append it to each list item
-var myNodelist = document.getElementsByTagName("LI");
-var i;
-for (i = 0; i < myNodelist.length; i++) {
-  var span = document.createElement("SPAN");
-  var txt = document.createTextNode("\u00D7");
+// var myNodelist = document.getElementsByTagName("LI");
+// var i;
+// for (i = 0; i < myNodelist.length; i++) {
+//   var span = document.createElement("SPAN");
+//   var txt = document.createTextNode("\u00D7");
+//   span.className = "close";
+//   span.appendChild(txt);
+//   myNodelist[i].appendChild(span);
+// }
+
+// todo 텍스트 목록을 추가한다.
+let myNodelist = document.getElementsByTagName("LI");
+// !매번 length에 접근하면 성능이슈가 있으므로 고정적 길이를 선언한다.
+// ! i를 for밖에 선언하면 에러가 발생한다.. 이유는? 전역 스코프이기 때문에 밑에서 재 선언을 하면 에러다. for문은 지역 스코프이기 때문에 에러가 안난다.
+for (let i = 0, loopCnt = myNodelist.length; i < loopCnt; i++) {
+  let span = document.createElement("SPAN");
+  let txt = document.createTextNode("\u00D7");
   span.className = "close";
   span.appendChild(txt);
   myNodelist[i].appendChild(span);
 }
 
+////////////////////////////////////////////////////////////////////////////
+
 // Click on a close button to hide the current list item
 var close = document.getElementsByClassName("close");
 var i;
 for (i = 0; i < close.length; i++) {
-  close[i].onclick = function() {
+  close[i].onclick = function () {
     var div = this.parentElement;
     div.style.display = "none";
   }
@@ -22,7 +40,7 @@ for (i = 0; i < close.length; i++) {
 
 // Add a "checked" symbol when clicking on a list item
 var list = document.querySelector('ul');
-list.addEventListener('click', function(ev) {
+list.addEventListener('click', function (ev) {
   if (ev.target.tagName === 'LI') {
     ev.target.classList.toggle('checked');
   }
@@ -48,7 +66,7 @@ function newElement() {
   li.appendChild(span);
 
   for (i = 0; i < close.length; i++) {
-    close[i].onclick = function() {
+    close[i].onclick = function () {
       var div = this.parentElement;
       div.style.display = "none";
     }
